a15d669450526ac682ac92fb080b877823bbacc1
Nathaniel Miller <dukeoflaser@gmail.com>
2016-02-27 03:17:56 +0000

    Add CD, Coupon, and NinetiesTeen classes

--- lib/nineties_teen.rb
+++ lib/nineties_teen.rb
@@ -1,4 +1,28 @@
 class NinetiesTeen
-
+  attr_accessor :name, :cd_collection
   
+  def initialize(name)
+    @name = name
+    @cd_collection = []
+  end
+  
+  def my_cd_collection
+    cd_collection.each do |cd|
+      puts "#{cd.artist}: #{cd.title}"
+    end
+  end
+  
+  def buy_cd(cd)
+    cd_collection << cd
+    cd.teen = self
+    new_coupon = Coupon.new
+    cd.coupon = new_coupon
+    new_coupon.cd = cd
+  end
+  
+  def my_coupons
+    cd_collection.collect do |cd|
+      cd.coupon
+    end
+  end  
 end
